acff96e73319bf773e5717808ad4058b8808cfdf
Olha Tkach <olja199807@gmail.com>
2022-02-01 21:21:11 +0200

    Game

---
+++ app.js
@@ -0,0 +1,95 @@
+const startBtn = document.querySelector("#start");
+const screens = document.querySelectorAll(".screen");
+const timeList = document.querySelector("#time-list");
+const timeEl = document.querySelector("#time");
+const board = document.querySelector("#board");
+const colors = [
+  "aqua",
+  "yellow",
+  "white",
+  "green",
+  "purple",
+  "rgb(199, 98, 202)",
+  "rgb(89, 11, 92)",
+  "rgb(89, 11, 92)",
+  "tomato",
+];
+let time = 0;
+let score = 0;
+startBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  screens[0].classList.add("up");
+});
+timeList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("time-btn")) {
+    time = parseInt(e.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
+});
+board.addEventListener("click", (e) => {
+  if (e.target.classList.contains("circle")) {
+    score++;
+    e.target.remove();
+    createRandomCircle();
+  }
+});
+
+function startGame() {
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+  setTime(time);
+}
+function decreaseTime() {
+  if (time === 0) {
+    finishGame();
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTime(current);
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`;
+}
+function finishGame() {
+  timeEl.parentNode.classList.add("hide");
+  board.innerHTML = `<h1>Result:<span class='primary'>${score}</span></h1>  `;
+}
+function createRandomCircle() {
+  const circle = document.createElement("div");
+  const size = getRandomNumber(10, 60);
+  const color = getRandomColor();
+  const { width, height } = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  circle.style.background = color;
+  board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+// хакнути гру
+function winTheGame() {
+  function kill() {
+    const circle = document.querySelector(".circle");
+
+    if (circle) {
+      circle.click();
+    }
+  }
+  setInterval(kill, 42);
+}
